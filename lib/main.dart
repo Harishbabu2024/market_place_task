35ed02ffbb238c5d056f32babb5955ccc0c85e8c
harishbabu2201 <medleiz.developer@gmail.com>
2025-09-17 09:30:28 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:market_place_task/view/list_page.dart';
-import 'package:market_place_task/view/single_post_view_page.dart';
 import 'package:market_place_task/widget/bottom_nav_page.dart';
-import 'package:market_place_task/widget/dummy.dart';
 
 void main() {
   runApp(const MyApp());
